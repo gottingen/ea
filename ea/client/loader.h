bd216412e202870abf346247f494a9ed65aa1a77
jeff.li <lijippy@163.com>
2023-12-02 14:24:54 +0800

    update docs

--- ea/client/loader.h
+++ ea/client/loader.h
@@ -24,57 +24,57 @@
 #include <google/protobuf/descriptor.h>
 #include "eapi/servlet/servlet.interface.pb.h"
 
+/**
+ * @defgroup ea_proto_g proto  proto operators
+ *
+ */
+
+namespace EA::client {
+
+    /**
+     * @ingroup ea_proto_g
+     * \rst
+     * @brief Loader is a helper class for proto convert from json to protobuf.
+     *        do not ignore the result of function, it is recommend use like:
+     *        auto json_config = "R{
+     *             "name": "example",
+     *             "version": {
+     *               "major": 1,
+     *               "minor": 2,
+     *               "patch": 3
+     *             },
+     *             "content": "{\"servlet\":\"sug\",\"zone\":{\"instance\":[\"192.168.1.2\",\"192.168.1.3\",\"192.168.1.3\"],\"name\":\"ea_search\",\"user\":\"jeff\"}}",
+     *             "type": "CF_JSON",
+     *             "time": 1701477509
+     *           }"
+     *        EA::servlet::ConfigInfo info;
+     *        auto status = Loader::load_proto(json_config, info);
+     *        if(!status.ok) {
+     *          handler_error();
+     *         }
+     *         ...
+     *         handler_success();
+     * \endrst
+     */
+    class Loader {
+    public:
+
         /**
-         * @defgroup ea_proto_g proto  proto operators
-         *
+         * @brief load a json format string to the message
+         * @param content [input] json format string.
+         * @param message [output] result of parse the json format string.
+         * @return status.ok() if success else return the reason of parse fail.
          */
+        static turbo::Status load_proto(const std::string &content, google::protobuf::Message &message);
 
-namespace EA {
-    namespace client {
         /**
-         * @ingroup ea_proto_g
-         * @brief Loader is a helper class for proto convert from json to protobuf.
-         *        do not ignore the result of function, it is recommend use like:
-         *        auto json_config = "R{
-         *             "name": "example",
-         *             "version": {
-         *               "major": 1,
-         *               "minor": 2,
-         *               "patch": 3
-         *             },
-         *             "content": "{\"servlet\":\"sug\",\"zone\":{\"instance\":[\"192.168.1.2\",\"192.168.1.3\",\"192.168.1.3\"],\"name\":\"ea_search\",\"user\":\"jeff\"}}",
-         *             "type": "CF_JSON",
-         *             "time": 1701477509
-         *           }"
-         *        EA::servlet::ConfigInfo info;
-         *        auto status = Loader::load_proto(json_config, info);
-         *        if(!status.ok) {
-         *          handler_error();
-         *         }
-         *         ...
-         *         handler_success();
          *
+         * @param path [input]file path, the file content must be json format.
+         * @param message [output] result of parse the json format string.
+         * @return status.ok() if success else return the reason of parse fail.
          */
-        class Loader {
-        public:
-
-            /**
-             * @brief load a json format string to the message
-             * @param content [input] json format string.
-             * @param message [output] result of parse the json format string.
-             * @return status.ok() if success else return the reason of parse fail.
-             */
-            static turbo::Status load_proto(const std::string &content, google::protobuf::Message &message);
-
-            /**
-             *
-             * @param path [input]file path, the file content must be json format.
-             * @param message [output] result of parse the json format string.
-             * @return status.ok() if success else return the reason of parse fail.
-             */
-            static turbo::Status load_proto_from_file(const std::string &path, google::protobuf::Message &message);
-        };
-    }  // namespace EA::client
-}
+        static turbo::Status load_proto_from_file(const std::string &path, google::protobuf::Message &message);
+    };
+}  // namespace EA::client
 
 #endif  // EA_CLIENT_LOADER_H_
